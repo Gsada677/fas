f14d508d0fe0f721f52a4863cab3a9a94cb679f5
Gsada677 <gsada7083@gmail.com>
2026-03-27 18:15:00 +0600

    returnn dz6

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:gus/Caffold.dart';
 import 'package:gus/app_database.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 late final AppDatabase appDatabase;
-void main() {
+void main()async {
     Future<bool>Check()async{
     final prefs=await
     SharedPreferences.getInstance();
     return prefs.getBool('SeenOnbrd', )??false;
-  }
+    }
+    
   void mainn()async{
   WidgetsFlutterBinding.ensureInitialized();
   final seenn = await Check();
